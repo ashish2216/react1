51daadbf0ba8daebf34756d333bb91498bf49b11
Ashish Verma <ashish.verma@crownstack.com>
2021-10-08 13:33:51 +0530

    Added new greetings

--- src/page0/Greeting.js
+++ src/page0/Greeting.js
@@ -4,25 +4,32 @@ let greet = "";
 let tate = new Date();
 const head = {};
 if (tate.getHours() >= 4 && tate.getHours() < 12) {
-  greet = "Good Morning";
+  greet = "Good Morning, I wish Your day will so osm";
   head.color = "green";
 } else if (tate.getHours() >= 12 && tate.getHours() < 19) {
-  greet = "Good Afteernoon";
+  greet = "Good Afteernoon, Let's go out for a lunch";
   head.color = "orange";
 } else {
-  greet = "Good Night";
+  greet = "Good Night, Sleep tight";
   head.color = "black";
 }
+
 function greeting() {
-  return (
-    <>
-      <div className="greetdiv">
-        <h1>
-          {" "}
-          Hello Sir, <span style={head}>{`${greet}`}</span>
-        </h1>
-      </div>
-    </>
+  return ( <
+    >
+    <
+    div className = "greetdiv" >
+    <
+    h1 > {
+      " "
+    }
+    Hello Sir, < span style = {
+      head
+    } > {
+      `${greet}`
+    } < /span> < /
+    h1 > <
+    /div> < / >
   );
 }
 export default greeting;
